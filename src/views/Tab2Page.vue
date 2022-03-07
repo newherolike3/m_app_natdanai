0798031dde7d34a6e6e1bf13469cb807f732d752
Natdanai Tinnakon <newsnail081@gmail.com>
2022-03-07 12:02:15 +0700

    first commit

---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,80 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>ข่าวสาร</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-card v-for="(news, i) in this.dataNews" :key="i">
+        <img :src="news.urlToImage" />
+        <ion-item>
+          <ion-icon :icon="pin" slot="start"></ion-icon>
+          <ion-label>{{ news.title }}</ion-label>
+          <ion-button :href="news.url" fill="outline" slot="end"
+            >View</ion-button
+          >
+        </ion-item>
+
+        <ion-card-content>
+          {{ news.description }}
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonCard,
+  IonCardContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from "@ionic/vue";
+import { pin } from "ionicons/icons";
+import axios from "axios";
+
+export default {
+  name: "Tab2Page",
+  components: {
+    IonCard,
+    IonCardContent,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonButton,
+  },
+  setup() {
+    return { pin };
+  },
+  data() {
+    return {
+      dataNews: null,
+    };
+  },
+  mounted() {
+    this.loadNews();
+  },
+  methods: {
+    async loadNews() {
+      const res = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=th&apiKey=fa9cf64d8d414b03a0b4c8e50c016817"
+      );
+      this.dataNews = res.data.articles;
+      console.log(this.dataNews);
+    },
+  },
+};
+</script>
